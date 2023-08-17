3a5c5b835039d3cfb430e6ed9c9f098b25d1e374
diegowh <contacto.diegowh@gmail.com>
2023-08-17 17:52:26 +0200

    Creados botones de busqueda para no tener que introducir manualmente el path al archivo o directorio

--- pdf_splitter_app.py
+++ pdf_splitter_app.py
@@ -1,5 +1,6 @@
 import os
 from tkinter import *
+from tkinter import filedialog
 from pdf_splitter import PDFSplitter
 
 class PDFSplitterApp(Tk):
@@ -12,15 +13,32 @@ class PDFSplitterApp(Tk):
         
         self.pdf_file_label = Label(self, text="PDF file:")
         self.pdf_file_entry = Entry(self, textvariable=self.pdf_file_path)
+        self.pdf_file_browse_button = Button(self, text="Browse", command=self.browse_pdf_file)
+        
         self.output_directory_label = Label(self, text="Output directory:")
         self.output_directory_entry = Entry(self, textvariable=self.output_directory)
+        self.output_directory_browse_button = Button(self, text="Browse", command=self.browse_output_directory)
+        
         self.split_button = Button(self, text="Split", command=self.split_pdf)
         
         self.pdf_file_label.pack(pady=10)
         self.pdf_file_entry.pack(pady=5)
+        self.pdf_file_browse_button.pack(pady=5)
+        
         self.output_directory_label.pack(pady=10)
         self.output_directory_entry.pack(pady=5)
+        self.output_directory_browse_button.pack(pady=5)
+        
         self.split_button.pack(pady=20)
+        
+                
+    def browse_pdf_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
+        self.pdf_file_path.set(file_path)
+        
+    def browse_output_directory(self):
+        directory = filedialog.askdirectory()
+        self.output_directory.set(directory)
         
     def split_pdf(self):
         pdf_file_path = self.pdf_file_path.get()
