4b79e4a62ef6686476e4c833b1703d124b52d9f6
diegowh <contacto.diegowh@gmail.com>
2023-08-22 13:44:46 +0200

    Crea docstrings y refactoriza un atributo para mejorar su legibilidad y coherencia con snake_case.

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+"""
+PDF Splitter App
+This is a GUI application for splitting PDF pages.
+"""
+
 from gui import UserInterface
 
 class PDFSplitter:
+    """Represents a controller for the PDFSplitter app.
+    """
     def __init__(self) -> None:
-        self.ui = UserInterface()
-    
+        self.user_interface = UserInterface()
+
     def run(self):
-        self.ui.mainloop()
+        """Run the PDFSplitter app.
+        """
+        self.user_interface.mainloop()
 
 if __name__ == "__main__":
     PDFSplitter().run()
