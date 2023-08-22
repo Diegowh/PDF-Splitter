aeb82397b1be6c06af8b03198e87f7c7c2b39a03
diegowh <contacto.diegowh@gmail.com>
2023-08-22 13:11:08 +0200

    Crea docstrings para los metodos de la clase

--- pdf_splitter_app.py
+++ pdf_splitter_app.py
@@ -29,12 +29,12 @@ class PDFSplitterApp(tk.Tk):
             "pdf_file": tk.Label(self, text="PDF file:"),
             "output_directory": tk.Label(self, text="Output directory:")
         }
-        
+
         self.entries = {
             "pdf_file": tk.Entry(self, textvariable=self.paths["pdf_file_path"]),
             "output_directory": tk.Entry(self, textvariable=self.paths["output_directory"])
         }
-        
+
         self.buttons = {
             "browse_pdf": tk.Button(self, text="Browse", command=self.browse_pdf_file),
             "browse_output": tk.Button(self, text="Browse", command=self.browse_output_dir),
@@ -53,14 +53,20 @@ class PDFSplitterApp(tk.Tk):
 
 
     def browse_pdf_file(self):
+        """Open a file dialog to browse for a PDF file.
+        """
         file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
         self.paths["pdf_file_path"].set(file_path)
 
     def browse_output_dir(self):
+        """Open a directory dialog to choose an output directory.
+        """
         directory = filedialog.askdirectory()
         self.paths["output_directory"].set(directory)
 
     def split_pdf(self):
+        """Split the selected PDF file and save the parts to the chosen directory.
+        """
         pdf_file_path = self.paths["pdf_file_path"].get()
         output_directory = self.paths["output_directory"].get()
 
@@ -73,8 +79,6 @@ class PDFSplitterApp(tk.Tk):
         pdf_splitter = PDFSplitter(pdf_file_path)
 
         pdf_splitter.split_file(output_directory)
-
-        print("File successfully splitted.")
 
 if __name__ == "__main__":
     app = PDFSplitterApp()
